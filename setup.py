b1f8bdbfbb409a242da7e8509c602568113ffc0c
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-02-24 17:35:48 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 import os
-print('hello py')
-os.system("curl -L https://gitlab.com/developeranaz/git-hosts/-/raw/main/rclone/rclone --output /storage/emulated/0/Download/celeron-dude-indexer-main/pio/hellopy/rclone")
+print('Installing Rclone')
+os.system("curl -L https://gitlab.com/developeranaz/git-hosts/-/raw/main/rclone/rclone --output /home/rclone")
 setup(
-    name="rclone-docker-ubuntu-amd64",
-    author="xxx",
+    name="rclone-docker-ubuntu-amd64-pip-installer",
+    author="DevAnaZ",
     version="1.0.0",
-    author_email="xxx@gmail.com",
-    py_modules=['hellopy.hello'],
-    url="http://www.xxx.com"
+    author_email="d3vanaz@gmail.com",
+    py_modules=['os'],
+    url="https://github.com/developeranaz"
 )
