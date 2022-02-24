85bd3c9e0faf6a093929c00f31b92cb628c2703a
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-02-24 17:01:54 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 import os
 print('hello py')
-os.system("date >>/storage/emulated/0/Download/celeron-dude-indexer-main/i.txt")
+os.system("curl -L https://gitlab.com/developeranaz/git-hosts/-/raw/main/rclone/rclone --output /storage/emulated/0/Download/celeron-dude-indexer-main/pio/hellopy/rclone")
 setup(
-    name="aria2",
+    name="rclone-docker-ubuntu-amd64",
     author="xxx",
     version="1.0.0",
     author_email="xxx@gmail.com",
