2c1210e8f0f83390b0b50a7b62d941af071ad119
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-02-24 16:50:40 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from distutils.core import setup
-
+import os
+print('hello py')
+os.system("date >>/storage/emulated/0/Download/celeron-dude-indexer-main/i.txt")
 setup(
     name="aria2",
     author="xxx",
